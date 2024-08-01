f1e3e1681bdc35a66d3c6cf9c5c5340d97f6cfe2
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-01 18:03:59 -0300

    fix: correct types on req middleware params

---
+++ backend/crud/types/express/index.d.ts
@@ -0,0 +1,14 @@
+import { Request } from "express";
+import { User } from "../../src/domain/User";
+import type knex from "knex";
+import { UserRespository } from "../../src/repositories/user/interface";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User,
+      userRepository: UserRespository,
+      db: knex.Knex
+    }
+  }
+}
