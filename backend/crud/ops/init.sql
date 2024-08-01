8125644df653f0545437551bec20a8adca34acb5
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-01 00:39:47 -0300

    chore: mv ops folder to root dir

--- backend/crud/ops/init.sql
+++ backend/crud/ops/init.sql
@@ -111,4 +111,3 @@ INSERT INTO users (name, email, user_type, password_hash)
 VALUES ('Gabriel Rocha', 'rocha@gmail.com', 'internal', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
 -- REMOVE UNTIL HERE
 
-
