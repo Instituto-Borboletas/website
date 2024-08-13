80db2f17d50f4bdcce43b73cbbc98971ad84952c
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-13 19:06:27 -0300

    feat: finishing the create user modal

--- frontend/src/components/views/UsersView.jsx
+++ frontend/src/components/views/UsersView.jsx
@@ -12,16 +12,70 @@ import {
   ModalCloseButton,
   FormControl,
   FormLabel,
+  useToast,
 } from "@chakra-ui/react"
 import { BiPlus } from "react-icons/bi"
+import { useQuery } from '@tanstack/react-query'
 
+import { crudApi } from "../../utils/api";
 import { useDisclosure } from "../../hooks/disclosure";
+import { PasswordInput } from "../PasswordInput";
 
 export function UsersView() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  // const { isPending, error, data, isLoading } = useQuery({
+  //   queryKey: ["internalUsersListing"],
+  //   queryFn: () =>
+  //     crudApi.get("/users"),
+  // })
 
-  function handleSubmit ({ email, password }) {
+  const toast = useToast();
+
+  async function handleSubmit ({ name, email, password, confirmationPassword }) {
+    if (!name || !email || !password || !confirmationPassword) {
+      setErrorMessage("Preencha todos os campos");
+      return;
+    }
+
+    if (password !== confirmationPassword) {
+      setErrorMessage("As senhas não coincidem");
+      return;
+    }
+
+    setErrorMessage(null);
+
+    try {
+      setIsLoading(true);
+      // const response = await crudApi.post("/users", {
+      //   name,
+      //   email,
+      //   password,
+      //});
+      await new Promise(resolve => setTimeout(resolve, 2000));
+
+      toast({
+        title: "Usuário criado com suscesso!",
+        description: "O novo usuário já pode acessar o sistema.",
+        position: "top",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      })
+    } catch (error) {
+      if (error.response.status === 409) {
+
+      }
+    } finally {
+      setIsLoading(false);
+      handleClose();
+    }
+  }
+
+  function handleClose () {
+    setErrorMessage(null);
+    onClose();
   }
 
   return (
@@ -49,7 +103,7 @@ export function UsersView() {
                 <th className="border w-1/4 text-lg">Tipo</th>
               </tr>
             </thead>
-            <tbody className="bg-white text-center max-h-[44rem] overflow-y-auto block w-full">
+            <tbody className="bg-white text-center max-h-[38rem] overflow-y-auto block w-full">
               {
                 Array(20).fill().map(() => (
                   <tr key={Math.random()} className="flex w-full">
@@ -77,14 +131,35 @@ export function UsersView() {
           </span>
         </Button>
 
-        <CreateUserModal isOpen={isOpen} onCancel={onClose} onSubmit={handleSubmit} errorMessage={errorMessage} />
+        <CreateUserModal
+          isOpen={isOpen}
+          onCancel={onClose}
+          onSubmit={handleSubmit}
+          onChange={() => setErrorMessage(null)}
+          errorMessage={errorMessage}
+          isLoading={isLoading}
+        />
       </footer>
     </section>
   )
 }
 
-function CreateUserModal({ isOpen, onCancel, onSubmit, errorMessage }) {
+function CreateUserModal({ isOpen, onCancel, onSubmit, onChange, errorMessage, isLoading }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
   const initialRef = useRef();
+
+  async function submitForm () {
+    await onSubmit({ name, email, password, confirmationPassword: passwordConfirmation });
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    setPasswordConfirmation("");
+  }
 
   return (
     <Modal
@@ -100,24 +175,49 @@ function CreateUserModal({ isOpen, onCancel, onSubmit, errorMessage }) {
         <ModalHeader>Criar usuário interno</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          {
+            errorMessage && (
+              <div className="bg-red-200 text-red-800 p-2 mb-4 rounded">
+                {errorMessage}
+              </div>
+            )
+          }
           <FormControl>
             <FormLabel>Nome completo</FormLabel>
-            <Input ref={initialRef} placeholder="Seu nome e sobrenome" />
+            <Input
+              ref={initialRef}
+              placeholder="Seu nome e sobrenome"
+              required
+              value={name}
+              onChange={({ target }) => { setName(target.value); onChange() }}
+            />
           </FormControl>
 
           <FormControl mt={4}>
             <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="seuemail@gmail.com" />
+            <Input
+              type="email"
+              placeholder="seuemail@gmail.com"
+              required
+              value={email}
+              onChange={({ target }) => { setEmail(target.value); onChange(); }}
+            />
           </FormControl>
 
           <FormControl mt={4}>
             <FormLabel>Senha</FormLabel>
-            <Input type="password"/>
+            <PasswordInput
+              value={password}
+              onChange={({ target }) => { setPassword(target.value); onChange(); }}
+            />
           </FormControl>
 
           <FormControl mt={4}>
             <FormLabel>Repita a senha</FormLabel>
-            <Input type="password"/>
+            <PasswordInput
+              value={passwordConfirmation}
+              onChange={({ target }) => { setPasswordConfirmation(target.value); onChange(); }}
+            />
           </FormControl>
         </ModalBody>
 
@@ -130,10 +230,11 @@ function CreateUserModal({ isOpen, onCancel, onSubmit, errorMessage }) {
           </Button>
           <Button
             colorScheme="blue"
-            mr={3}
-            onClick={onSubmit}
+            ml={3}
+            onClick={submitForm}
+            isDisabled={errorMessage || isLoading}
           >
-            Criar
+            { isLoading ? "Criando..." : "Criar" }
           </Button>
         </ModalFooter>
       </ModalContent>
