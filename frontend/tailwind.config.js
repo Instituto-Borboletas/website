1d96bf4efa2865f784ec31fee9c1e55a74be86f9
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-08-30 23:42:11 -0300

    chore(frontend): setup for date-picker component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,30 @@
+import flowbite from "flowbite-react/tailwind";
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#56B2CF",
-        secondary: "#388EA9",
+        primary: '#56B2CF',
+        secondary: '#388EA9'
       },
-    },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    animate,
+    flowbite.plugin(),
+  ],
 }
 
