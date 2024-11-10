3fed750cb94bbb9dd5635117bc07edf770ca6bf4
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-11-10 19:41:28 -0300

    feat: make home page responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,17 +1,17 @@
 export function Footer () {
   return (
     <footer className="bg-primary text-white">
-      <div className="flex flex-col items-center justify-between w-5/6 mx-auto py-6">
-        <h2 className="text-2xl font-bold">Contatos</h2>
+      <div className="flex flex-col items-center justify-between md:w-5/6 mx-auto py-6">
+        <h2 className="text-xl md:text-2xl font-bold">Contatos</h2>
 
         <ul className="flex space-x-4 py-4">
           { /* TODO: adicionar email e telefone de contato */}
+          <li> <a target="_blank" href="https://www.instagram.com/instituto.borboleta/" className="underline">Instagram</a> </li>
           <li> <a target="_blank" href="mailto:" className="underline">Email</a> </li>
           <li> <a target="_blank" href="tel:" className="underline">Telefone</a> </li>
-          <li> <a target="_blank" href="https://www.instagram.com/instituto.borboleta/" className="underline">Instagram</a> </li>
         </ul>
 
-        <p>&copy; 2024 Instituto Borboletas. Todos os direitos reservados.</p>
+        <p className="text-sm md:text-md">&copy; 2024 Instituto Borboletas. Todos os direitos reservados.</p>
       </div>
     </footer>
   )
