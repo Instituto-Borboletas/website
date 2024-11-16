d7670165743b5ffe4b13f35b1ff112e7482bc5f7
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-11-15 23:22:10 -0300

    feat: dasboard cards change page on click

--- frontend/src/components/views/DashboardView.jsx
+++ frontend/src/components/views/DashboardView.jsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom"
+
 export function DashboardView () {
   return (
     <section className="flex flex-col p-10">
       <header className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-10">
-        <div className="bg-white p-5 rounded shadow-md">
+        <Link reloadDocument to="/interno?pagina=ajudas" className="bg-white p-5 rounded shadow-md">
           <h2 className="text-lg font-bold">Pedidos de ajuda</h2>
           <p className="text-3xl text-primary mt-2">5</p>
-        </div>
+        </Link>
 
-        <div className="bg-white p-5 rounded shadow-md">
+        <Link reloadDocument to="/interno?pagina=voluntarios" className="bg-white p-5 rounded shadow-md">
           <h2 className="text-lg font-bold">Voluntários</h2>
           <p className="text-3xl text-primary mt-2">3</p>
-        </div>
+        </Link>
 
-        <div className="bg-white p-5 rounded shadow-md">
+        <Link reloadDocument to="/interno?pagina=usuarios" className="bg-white p-5 rounded shadow-md">
           <h2 className="text-lg font-bold">Usuários registrados</h2>
           <p className="text-3xl text-primary mt-2">10</p>
-        </div>
+        </Link>
       </header>
 
       <main className="w-full">
