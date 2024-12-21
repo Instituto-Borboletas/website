71394cb52b49ef39249db128bcb06f7e21268d01
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-12-20 22:08:28 -0300

    refact: init react router (as framework) project

---
+++ app/tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+  	extend: {
+      colors: {
+        primary: '#56B2CF',
+        secondary: '#388EA9'
+      },
+  		fontFamily: {
+  			sans: [
+  				'Inter"',
+  				'ui-sans-serif',
+  				'system-ui',
+  				'sans-serif',
+  				'Apple Color Emoji"',
+  				'Segoe UI Emoji"',
+  				'Segoe UI Symbol"',
+  				'Noto Color Emoji"'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
